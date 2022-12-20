59a627ab31ff99066ad1a96ce857259d95dfc598
neetesshhr <rijalboy.94@gmail.com>
2022-12-20 11:50:37 +0545

    ignores file deletion

--- buttonclicker.js
+++
@@ -1,8 +0,0 @@
-var button = document.getElementsByClassName("af18dbd5a4")
-
-button.addEventListener("click", function(){
-    var ws = new WebSocket("ws://localhost:4321")
-    ws.onopen = function() {
-        ws.send("fetch_url");
-    }
-})
